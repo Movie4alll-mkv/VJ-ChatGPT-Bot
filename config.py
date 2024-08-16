26231ff645885e4aa7a58bd11142c7028e0794ad
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2024-08-16 18:35:48 +0500

    Update config.py

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ def is_enabled(value, default):
     else:
         return default
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
-DB_URI = environ.get("DB_URI", "")
+API_ID = int(environ.get("API_ID", "6534707"))
+API_HASH = environ.get("API_HASH", "4bcc61d959a9f403b2f20149cbbe627a")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7509328585:AAFpxHizMnpeY6I9O6QvAljmG7lIGvgCc9Y")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002244364312"))
+ADMINS = int(environ.get("ADMINS", "1430593323"))
+DB_URI = environ.get("DB_URI", "mongodb+srv://nhere118:jSsmgEs1uEqnlbrZ@cluster0.rdkih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "chatgptvjbot")
 OPENAI_API = environ.get("OPENAI_API", "")
 AI = is_enabled((environ.get("AI","True")), False)
